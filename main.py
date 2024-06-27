b848ce0ed02e137bf3582226f455e83cfd388149
weeeiiii <amendawei900528@gmail.com>
2024-06-27 18:16:21 +0800

    627

--- main.py
+++ main.py
@@ -1,53 +1,40 @@
-from collections import defaultdict
-
-class Graph:
+class BaguaghostLeg:
     def __init__(self):
-        self.graph = defaultdict(list)
+        self.horizontal_lines = {}
 
-    def addEdge(self, u, v):
-        self.graph[u].append(v)
-    
-    def DFSUtil(self, v, visited, path):
-        visited.add(v)
-        path.append(v)
-        
-        for neighbour in self.graph[v]:
-            if neighbour not in visited:
-                self.DFSUtil(neighbour, visited, path)
-    
-    def DFS(self, v):
-        visited = set()
-        path = []
-        self.DFSUtil(v, visited, path)
-        return path
+    def add_line(self, i, j, x):
+        if x not in self.horizontal_lines:
+            self.horizontal_lines[x] = {}
+        self.horizontal_lines[x][(i, j)] = True
+        self.horizontal_lines[x][(j, i)] = True
 
-def build_bagua_graph(filename):
-    g = Graph()
-    with open(filename, 'r') as file:
-        for line in file:
-            line = line.strip()
-            if line:  # 忽略空行
-                parts = line.split(', ')
-                if len(parts) == 3:  # 确保有3个值
-                    i, j, x = parts
-                    i, j = int(i), int(j)
-                    g.addEdge((i, x), (j, x))
-                    g.addEdge((j, x), (i, x))
-    return g
+    def get_final_position(self, start):
+        current_pos = start
+        for x in ['r', 's', 't', 'u', 'v']:
+            if x in self.horizontal_lines:
+                if (current_pos, current_pos % 8 + 1) in self.horizontal_lines[x]:
+                    current_pos = current_pos % 8 + 1
+                elif (current_pos, (current_pos - 2) % 8 + 1) in self.horizontal_lines[x]:
+                    current_pos = (current_pos - 2) % 8 + 1
+        return current_pos
+
+def read_input(file_path):
+    with open(file_path, 'r') as f:
+        lines = f.readlines()
+    bg_leg = BaguaghostLeg()
+    for line in lines:
+        if line.strip():
+            i, j, x = line.strip().split(',')
+            bg_leg.add_line(int(i), int(j), x.strip())
+    return bg_leg
+
+def main():
+    input_file = 'input.txt'
+    bg_leg = read_input(input_file)
+    results = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+    for i in range(1, 9):
+        final_pos = bg_leg.get_final_position(i)
+        print(f"No. {i} 參賽者之籤碼為[{results[final_pos - 1]}]")
 
 if __name__ == "__main__":
-    filename = 'input.txt'
-    g = build_bagua_graph(filename)
-    
-    start_points = [(i, 'Pr') for i in range(1, 9)]
-    outcomes = ['F', 'A', 'D', 'H', 'E', 'B', 'G', 'C']
-    
-    results = []
-    for i, start in enumerate(start_points):
-        path = g.DFS(start)
-        final_pos = path[-1][0]
-        result = f"No. {i + 1} 參賽者之籤碼為[ {outcomes[final_pos - 1]} ]"
-        results.append(result)
-    
-    for result in results:
-        print(result)
+    main()
