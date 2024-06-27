1fc5e1f9851108bc1ea6358d3641b2f650da14e3
weeeiiii <amendawei900528@gmail.com>
2024-06-27 18:06:59 +0800

    6/27

--- main.py
+++ main.py
@@ -1,40 +1,53 @@
 from collections import defaultdict
 
-#籤碼
-outcomes = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+class Graph:
+    def __init__(self):
+        self.graph = defaultdict(list)
 
-horizontal_edges = defaultdict(list)
+    def addEdge(self, u, v):
+        self.graph[u].append(v)
+    
+    def DFSUtil(self, v, visited, path):
+        visited.add(v)
+        path.append(v)
+        
+        for neighbour in self.graph[v]:
+            if neighbour not in visited:
+                self.DFSUtil(neighbour, visited, path)
+    
+    def DFS(self, v):
+        visited = set()
+        path = []
+        self.DFSUtil(v, visited, path)
+        return path
 
-def initialize_horizontal_edges(filename):
+def build_bagua_graph(filename):
+    g = Graph()
     with open(filename, 'r') as file:
         for line in file:
-            parts = line.strip().split(',')
-            i = int(parts[0])
-            j = int(parts[1])
-            x = parts[2].strip()
-            horizontal_edges[i].append((j, x))
-            horizontal_edges[j].append((i, x))
-
-# DFS
-def dfs(current_axis, current_branch, visited):
-    visited.add((current_axis, current_branch))
-
-
-    next_edges = sorted(horizontal_edges[current_axis], key=lambda x: x[1])
-    for next_axis, branch in next_edges:
-        if branch > current_branch and (next_axis, branch) not in visited:
-            return dfs(next_axis, branch, visited)
-    return current_axis
-
-def main():
-    filename = "input.txt" 
-    initialize_horizontal_edges(filename)
-
-    for i in range(1, 9):
-        visited = set()
-        result_axis = dfs(i, 'r', visited) 
-        result = outcomes[result_axis - 1]
-        print(f"No. {i} 参賽者之籤碼為[ {result} ]")
+            line = line.strip()
+            if line:  # 忽略空行
+                parts = line.split(', ')
+                if len(parts) == 3:  # 确保有3个值
+                    i, j, x = parts
+                    i, j = int(i), int(j)
+                    g.addEdge((i, x), (j, x))
+                    g.addEdge((j, x), (i, x))
+    return g
 
 if __name__ == "__main__":
-    main()
+    filename = 'input.txt'
+    g = build_bagua_graph(filename)
+    
+    start_points = [(i, 'Pr') for i in range(1, 9)]
+    outcomes = ['F', 'A', 'D', 'H', 'E', 'B', 'G', 'C']
+    
+    results = []
+    for i, start in enumerate(start_points):
+        path = g.DFS(start)
+        final_pos = path[-1][0]
+        result = f"No. {i + 1} 參賽者之籤碼為[ {outcomes[final_pos - 1]} ]"
+        results.append(result)
+    
+    for result in results:
+        print(result)
